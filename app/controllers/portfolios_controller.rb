5acce9f55125cb87eebfae1f7a81ac3416fb512d
Ken Hirata <ken.hirata@gmail.com>
2020-03-15 16:12:45 -0700

    add show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -30,6 +30,10 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   # def destroy
   #   @portfolio_item = Portfolio.find(params[:id])
   #   @portfolio_item.destroy
